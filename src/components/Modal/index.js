b071221c56301147cb2dba6184ac5542c87345ab
ITDoctor40148 <zeus40148@hotmail.com>
2020-11-15 17:13:23 +0800

    testing

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -12,23 +12,23 @@ const Modal = ({ handleClose, show, links }) => {
         <div className="modal-close">x</div>
         <div>
           {links.length !== 0 && links[currentImageIndex].substring(0, 4) === "data" && <img src={links[currentImageIndex]} alt="Saved Data" />}
-          {links.length !== 0 && links[currentImageIndex].substring(0, 4) === "blob" && <video className="w-100" controls src={links[currentImageIndex]} title="Saved Data" >
-            <source src={links[currentImageIndex]} type="video/webm" />
-          </video>}
+          {links.length !== 0 && links[currentImageIndex].substring(0, 4) === "blob" &&
+            <video className="w-100" controls src={links[currentImageIndex]} title="Saved Data" >
+              <source src={links[currentImageIndex]} type="video/webm" />
+            </video>}
+          <div className="w-100 d-flex justify-content-between text-white display-6">
+            <div onClick={() => setCurrentImageIndex(Math.abs((currentImageIndex - 1) % links.length))}>&lt;</div>
+            <div onClick={() => setCurrentImageIndex((currentImageIndex + 1) % links.length)}>&gt;</div>
+          </div>
+          <button style={{
+            width: "100%",
+            bottom: "10px",
+            border: "0px",
+            backgroundColor: "#dd3b3b",
+            height: "50px",
+            color: "white"
+          }} onClick={() => handleClose()}>Back</button>
         </div>
-        <div className="w-100 d-flex justify-content-between text-white display-6">
-          <div onClick={() => setCurrentImageIndex(Math.abs((currentImageIndex - 1) % links.length))}>&lt;</div>
-          <div onClick={() => setCurrentImageIndex((currentImageIndex + 1) % links.length)}>&gt;</div>
-        </div>
-        <button style={{
-          width: "100%",
-          bottom: "10px",
-          position: "absolute",
-          border: "0px",
-          backgroundColor: "#dd3b3b",
-          height: "50px",
-          color: "white"
-        }} onClick={() => handleClose()}>Back</button>
       </section>
     </div>
   );
