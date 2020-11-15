ffc9609dfad1e229239100e37cc4cc0a718e3b3e
ITDoctor40148 <zeus40148@hotmail.com>
2020-11-15 17:28:42 +0800

    testing

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom'
 import { connect } from 'react-redux';
 import "./popupComponent.scss"
 const Modal = ({ handleClose, show, links }) => {
+  const video = React.useRef(null);
+  const [play, setPlay] = React.useState(false);
   const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
   const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  const toggle = React.useCallback(() => {
+    if (!play) {
+      video.current.play();
+      setPlay(true);
+    } else {
+      video.current.pause();
+      setPlay(false);
+    }
+  })
 
   return (
     <div className={showHideClassName}>
@@ -13,11 +25,14 @@ const Modal = ({ handleClose, show, links }) => {
         <div>
           {links.length !== 0 && links[currentImageIndex].substring(0, 4) === "data" && <img src={links[currentImageIndex]} alt="Saved Data" />}
           {links.length !== 0 && links[currentImageIndex].substring(0, 4) === "blob" &&
-            <video className="w-100" controls src={links[currentImageIndex]} title="Saved Data" >
+            <video ref={video} className="w-100" controls src={links[currentImageIndex]} title="Saved Data" >
               <source src={links[currentImageIndex]} type="video/webm" />
             </video>}
           <div className="w-100 d-flex justify-content-between text-white display-6">
             <div onClick={() => setCurrentImageIndex(Math.abs((currentImageIndex - 1) % links.length))}>&lt;</div>
+            <div onClick={() => {
+              toggle();
+            }}>Play</div>
             <div onClick={() => setCurrentImageIndex((currentImageIndex + 1) % links.length)}>&gt;</div>
           </div>
           <button style={{
